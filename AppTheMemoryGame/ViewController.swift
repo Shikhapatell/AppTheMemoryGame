6186d3109525b8967ac6b560903cb8cb38b10126
Shikha Patel <shikhapatel2003@gmail.com>
2020-06-24 19:00:06 -0500

    7

--- AppTheMemoryGame/ViewController.swift
+++ AppTheMemoryGame/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ @IBOutlet weak var GifView: UIImageView!
+    
+    @IBOutlet weak var GifView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+   
     }
 
 
